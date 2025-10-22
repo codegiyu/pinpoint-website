0aabbabc4655dd4e6ad1e908a4097a0d70c9611e
Edward-Precious Omegbu <eomegbu@gmail.com>
2025-10-22 19:27:25 +0100

    fix: redirect added to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      // Force HTTPS
+      // {
+      //   source: '/:path*',
+      //   has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+      //   destination: 'https://pinpoint.ng/:path*',
+      //   permanent: true,
+      // },
+      // Redirect www to root domain
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.pinpoint.ng' }],
+        destination: 'https://pinpoint.ng/:path*',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
